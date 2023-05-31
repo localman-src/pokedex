7e923aa9e17a65b2e5a2a3bf9b13213f1cd32f3c
localman-src <59379773+localman-src@users.noreply.github.com>
2023-05-31 16:30:39 -0400

    main flow denesting

--- main.go
+++ main.go
@@ -38,19 +38,20 @@ func main() {
 		command, err := config.Commands.getCommand(params[0])
 		if err != nil {
 			fmt.Printf("error: %s\n", err)
-		} else {
-			if len(params) == 1 {
-				err = command.callback(&config)
-				if err != nil {
-					fmt.Printf("error: %s\n", err)
-				}
-			} else {
-				err = command.callback(&config, params[1:]...)
-				if err != nil {
-					fmt.Printf("error: %s\n", err)
-				}
-			}
+			continue
+		}
 
+		if len(params) == 1 {
+			err = command.callback(&config)
+			if err != nil {
+				fmt.Printf("error: %s\n", err)
+			}
+			continue
+		}
+
+		err = command.callback(&config, params[1:]...)
+		if err != nil {
+			fmt.Printf("error: %s\n", err)
 		}
 
 	}
